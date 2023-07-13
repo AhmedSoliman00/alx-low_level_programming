e76ed3d8898bc1a012bf0b7d223de37eaa2d5766
AhmedSoliman00 <himozhima@gmail.com>
2023-07-13 16:54:37 -0700

    till number 4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+    /* Print a message to the console */
+    puts("\"Programming is like building a multilingual puzzle");
+
+    /* Return 0 to indicate successful completion of the program */
+    return (0);
+}
+
+
+
+
